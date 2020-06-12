4652315101e48a6a2db47c0d8a95c3be33adb255
Ian Robinson <ian@ianrobinson.net>
2020-06-12 12:01:48 +0700

    Add console.log('hello world') to myApp.js

--- myApp.js
+++ myApp.js
@@ -10,6 +10,7 @@ var bGround = require('fcc-express-bground');
 
 /** 1) Meet the node console. */
 bGround.log("Hello World");
+console.log("Hello World");
 
 /** 2) A first working Express Server */
 
