c4999f205bd6f5fb0bf67467b54da2ef4c785882
Ian Robinson <ian@ianrobinson.net>
2020-06-16 12:12:28 +0700

    Switch to arrow function

--- myApp.js
+++ myApp.js
@@ -70,7 +70,7 @@ app.get("/now", (req, res, next) => {
 
 /** 9)  Get input from client - Route parameters */
 
-app.get("/:word/echo", function(req, res) {
+app.get("/:word/echo", (req, res) => {
   res.json({ echo: req.params.word });
 });
 
